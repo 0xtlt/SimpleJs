f0314a0b556a7b4571275011c964354c1c408ce1
Thomas Tastet <31560900+ThomasT404@users.noreply.github.com>
2018-05-15 10:25:40 +0200

    let removed and add timeout for requests

--- simple.js
+++ simple.js
@@ -1,28 +1,31 @@
 var s = {
     post: function (url, data, callback) {
         if(data === undefined && callback === undefined){
-            let xmlHttp = new XMLHttpRequest();
+            xmlHttp = new XMLHttpRequest();
+            xmlHttp.timeout = 4000
             xmlHttp.open("POST", url, true);
             xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             xmlHttp.send(data);
         } else {
             if(callback === undefined){
-                let xmlHttp = new XMLHttpRequest();
+                xmlHttp = new XMLHttpRequest();
                 xmlHttp.onreadystatechange = function() {
                     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
                         data(xmlHttp.responseText);
                 }
 
+                xmlHttp.timeout = 4000
                 xmlHttp.open("POST", url, true);
                 xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 xmlHttp.send(data);
             } else {
-                let xmlHttp = new XMLHttpRequest();
+                xmlHttp = new XMLHttpRequest();
                 xmlHttp.onreadystatechange = function() {
                     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
                         callback(xmlHttp.responseText);
                 }
 
+                xmlHttp.timeout = 4000
                 xmlHttp.open("POST", url, true);
                 xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 xmlHttp.send(data);
@@ -31,24 +34,27 @@ var s = {
     },
     get: function (url, data, callback) {
         if(data === undefined && callback === undefined){
-            let xmlHttp = new XMLHttpRequest();
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.timeout = 4000
             xmlHttp.open("GET", url, true);
         } else {
             if(callback === undefined){
-                let xmlHttp = new XMLHttpRequest();
+                xmlHttp = new XMLHttpRequest();
                 xmlHttp.onreadystatechange = function() {
                     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
                         data(xmlHttp.responseText);
                 }
 
+                xmlHttp.timeout = 4000
                 xmlHttp.open("GET", url, true);
             } else {
-                let xmlHttp = new XMLHttpRequest();
+                xmlHttp = new XMLHttpRequest();
                 xmlHttp.onreadystatechange = function() {
                     if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
                         callback(xmlHttp.responseText);
                 }
 
+                xmlHttp.timeout = 4000
                 xmlHttp.open("GET", url, true);
                 xmlHttp.send(data);
             }
@@ -59,7 +65,7 @@ var s = {
             console.log('Error')
             return false
         } else {
-            let url = ''
+            url = ''
             for(i = 0; i <= json.length - 1; i++){
                 if(i === 0){
                     url += `${encodeURIComponent(json[i].name)}=${encodeURIComponent(json[i].value)}`
